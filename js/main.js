a22c3c1c2ed3ec98de2b1a40ba391ec711e534c2
ahmed-menisy <amelmaster16@gmail.com>
2022-10-15 15:33:35 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+import { Settings } from "./settings.js";
+const settings = new Settings();
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "../assets/particlesjs-config.json", function () {
+   console.log("callback - particles.js config loaded");
+   
+});
+
